828a7c955650787abe6ff2c50b27dbfa26cae728
tetreum <basura@yelidmod.com>
2013-04-14 20:39:12 +0300

    Working for Havij 1.15+1.16
    -Code cleaned
    -Use of regex
    -Deleted AIO query option
    -Deleted custom column names option (you may have to change it in your report html file)

--- 1135.php
+++ 1135.php
@@ -1,243 +1,149 @@
 <?php
-
 // HAVIJ REPORT EXPORTER (1135)
 // INITIAL VARS
-$debug_mode=false;
-$aio_query=true; // single insert order
-$sql_import=false;	// $to file will contain sql queries
-$array_import=false; // $data will contain the final array
-$all_queries=''; // DONT TOUCH IT
-//
+$sql=array();
+$config=array(
+	'to'=>'report.sql',
+	'file'=>'report.html',
+	'table'=>"exportedTable",
+	'createTable'=>true,
+);
+//---
 
-// SET PARAMS
-if(isset($_GET['to']))
-{	
-	$to_name=$_GET['to'];
+// Checking data
+if(isset($_GET['file'])){
+	$config['file']=$_GET['file'];
 }
-if(!isset($_GET['table'])){
-	$table_name='11_TABLE_NAME_35';
+if(isset($_GET['table'])){
+	$config['table']=$_GET['table'];
+}
+if(isset($_GET['to'])){
+	$config['to']=$_GET['to'];
+}
+
+
+
+if(!isset($config['file'])){
+	die('No file to import');
+}
+
+$content = file_get_contents($config['file']);
+
+if($content==''){
+	die('clean file given');
+}
+
+//Detect Havij version
+$p="/<font face=\"Verdana\" size=\"4\" color=\"#DC883D\">(.*)<br>/";
+preg_match($p, $content, $r);
+
+if(strpos($r[1],'1.15')!==false){
+	$config['version']='1.15';
 }else{
-	$table_name=mysql_escape_string($_GET['table']);
+	$config['version']='1.14';
 }
 
-if(isset($_GET['columns'])){	
-	$columns_list=$_GET['columns'];
+//Get columns
+$p="/<td nowrap bgcolor=\"#FFFFCE\"><b><font color=\"#DC883D\">(.*)<\/font><\/b><\/td>/";
+preg_match_all($p, $content, $r);
+
+$config['columns']=$r[1];
+if(sizeof($config['columns'])<1){
+	die('No columns found');
 }
 
-if(!isset($_GET['file'])){	
-	$file_name='NOT SET, EXPORT STOPPED';
-}else{
-	$file_name=$_GET['file'];
-}
-if(isset($_GET['aio']))
-{	
-	$aio_query=false;
-}
+//get data
+$p="/\<td bgcolor=\"\#FFF7F2\"\>(.*)\<\/td\>/";
+preg_match_all($p, $content, $r);
 
-echo'<pre>';
-//HEADER MESSAGE
-$message="-- HF HAVIJ REPORT EXPORTER (1135)
--- for larger exports, use it in localhost.
--- USAGE:
---	EXPORT TO FILE: 1135.php?file=HAVIJ_REPORT.html&table=YOUR_SQL_TABLE_NAME&to=export.sql
---	EXPORT TO ARRAY: edit 1135.php put array_import to true
---	SHOW QUERIES HERE (def): 1135.php?file=HAVIJ_REPORT.html&table=YOUR_SQL_TABLE_NAME
---	change column names: &columns=column1,column2,...
---	disable all-in-one query to get an INSERT for each line: &aio=0
--- File name: $file_name";
-if(isset($to_name)){
-	$message.="
--- To file: $to_name";
-}else{
-	$sql_import=true;
-}
-	$message.="
--- Table name: $table_name";
-	if(isset($columns_list)){
-	$message.="
--- Columns list: $columns_list";		
-	}
-$message.="
--- All-in-one query: $aio_query
--- Debug mode: $debug_mode
--- THIS LINES WILL BE IGNORED IN MYSQL
-
-";
-echo $message;
-$all_queries=$message;
-//--
-
-if($file_name=='NOT SET, EXPORT STOPPED'){die();}
-$content = file_get_contents($file_name);
-if ($content === false)
-   die('Failed when obtaining '.$file_name.', check perms, etc..');
-
-// GET COLUMNS
-$results=explode("#DC883D\">",$content);
-foreach($results as $id_result=>$column)
-{
-	if($id_result>3)
-	{
-		$bah=explode("</font></b></td>",$column);
-		$columns[]=$bah[0];
-		if($id_result==4){
-			$query_columns=$bah[0];
-		}else{
-			$query_columns.=','.$bah[0];
-		}
-	}
-}
-$results='';
-$total_columns=sizeof($columns);
-if($debug_mode){echo 'Columns: '.$total_columns;}
-if(isset($columns_list)){
-	$num_columns=sizeof(explode(",",$columns_list));
-	if($num_columns==$total_columns){
-		$query_columns=mysql_escape_string($columns_list);
-	}else{
-		die('ERROR: COLUMNS FOUND:'.$total_columns.' YOU LISTED:'.$num_columns);
-	}
-
-}
-// ---
-
-// GET DATA
 $i=0;
 $o=0;
-$results=explode("#FFF7F2\">",$content);
-unset($results[0]);
 
-foreach($results as $column_data)
+$columns=sizeof($config['columns']);
+foreach($r[1] as $data)
 {
-	$bah=explode("</td>",$column_data);
-	if($i<$total_columns)
-	{	
-		if($array_import){
-			$data[$o][$columns[$i]]=$bah[0];
-		}
-		if($i==0){
-			$query_data[$o]="'".mysql_escape_string($bah[0])."'";
-		}else{
-			$query_data[$o].=",'".mysql_escape_string($bah[0])."'";
-		}
-		$i++;
-	}else{
-		if($sql_import)
-		{
-			// SINGLE INSERT?
-			if($aio_query)
-			{
-				if($o==0){
-		echo "INSERT INTO $table_name ($query_columns) VALUES<br>($query_data[$o])";
-				}else{
-		echo ",<br>($query_data[$o])";
-				}
-
-			}else{
-				echo "INSERT INTO $table_name ($query_columns) VALUES ($query_data[$o]);<br>";
-			}
-		}
-		elseif(!$array_import)
-		{
-			if($aio_query)
-			{
-				if($o==0){
-		$all_queries.="
-INSERT INTO $table_name ($query_columns) VALUES
-($query_data[$o])";
-				}else{
-		$all_queries.=",
-($query_data[$o])";
-				}
-			}else{
-				$all_queries.="
-INSERT INTO $table_name ($query_columns) VALUES ($query_data[$o]);";
-			}
-
-		}
-		$o++;
+	$sql[$o][]=$data;
+	$i++;
+	if($i==$columns){
 		$i=0;
-		if($array_import){
-			$data[$o][$columns[$i]]=$bah[0];
-		}
-		$query_data[$o]="'".mysql_escape_string($bah[0])."'";
-		$i++;
+		$o++;
 	}
-
 }
+
+$p="/\<td bgcolor=\"\#FFFFFF\"\>(.*)\<\/td\>/";
+preg_match_all($p, $content, $r);
+
 $i=0;
 
-$results=explode("white\">",$content);
-unset($results[0]);
-
-foreach($results as $column_data)
+foreach($r[1] as $data)
 {
-	$bah=explode("</td>",$column_data);
-	if($i<$total_columns)
-	{	
-		if($array_import){
-			$data[$o][$columns[$i]]=$bah[0];
-		}
-		if($i==0){
-			$query_data[$o]="'".mysql_escape_string($bah[0])."'";
-		}else{
-			$query_data[$o].=",'".mysql_escape_string($bah[0])."'";
-		}
-		$i++;
-	}
-	else
-	{
-		if($sql_import)
-		{
-			// SINGLE INSERT?
-			if($aio_query)
-			{
-				if($o==0){
-		echo "INSERT INTO $table_name ($query_columns) VALUES<br>";
-				}
-				echo ",<br>($query_data[$o])";
-			}else{
-				echo "INSERT INTO $table_name ($query_columns) VALUES ($query_data[$o]);<br>";
-			}
-		}
-		elseif(!$array_import)
-		{
-			if($aio_query)
-			{
-				if($o==0){
-		$all_queries.="
-INSERT INTO $table_name ($query_columns) VALUES
-($query_data[$o])";
-				}else{
-		$all_queries.=",
-($query_data[$o])";
-				}
-			}else{
-				$all_queries.="
-INSERT INTO $table_name ($query_columns) VALUES ($query_data[$o]);";
-			}
-		}
-		$o++;
+	$sql[$o][]=$data;
+	$i++;
+	if($i==$columns){
 		$i=0;
-		if($array_import){
-			$data[$o][$columns[$i]]=$bah[0];
-		}
-		$query_data[$o]="'".mysql_escape_string($bah[0])."'";
-		$i++;
+		$o++;
+	}
+}
+
+// parse data to SQL
+
+$parser['content']="-- HF HAVIJ REPORT EXPORTER (1135)[on github.com]\n";
+
+if($config['createTable']){
+	$parser['content'].="
+--CHECK CREATE TABLE SCHEME BEFORE IMPORTING IT!!\n\n
+
+CREATE TABLE IF NOT EXISTS `".$config['table']."` (\n";
+}
+
+
+//columns line
+$parser['columns']='';
+foreach($config['columns'] as $column)
+{
+	$column=mysql_escape_string($column);
+	if($parser['columns']==''){
+		$parser['columns']=$column;
+		$primary=$column;
+	}else{
+		$parser['columns'].=','.$column;
 	}
 
+	if($config['createTable']){		
+		$parser['content'].="
+	`".$column."` varchar(120) DEFAULT NULL,\n";
+	}
 }
-$total_rows=$o+2; // +2 to look like havij report
-if($debug_mode){echo 'Columns: '.$total_rows;}
-//-
 
-// SAVE DATA
-if(!$sql_import && !$array_import)
+if($config['createTable']){	
+	$parser['content'].="
+PRIMARY KEY (`".$primary."`) \n) ENGINE=MyISAM DEFAULT CHARSET=latin1; \n\n";
+}
+// data
+
+foreach($sql as $rows)
 {
-	$triki=fopen($to_name,'w'); 
-	fwrite($triki, utf8_decode($all_queries));
+	$line='';
+	foreach($rows as $row)
+	{
+		$row=mysql_escape_string($row);
+		if(!is_numeric($row)){
+			$row="'".$row."'";
+		}
+		if($line==''){
+			$line=$row;
+		}else{
+			$line.=','.$row;
+		}		
+	}
+	$parser['content'].="
+	INSERT INTO ".$config['table']." (".$parser['columns'].") VALUES ($line);\n";
+}
+
+if(isset($config['to'])){
+	$triki=fopen($config['to'],'w'); 
+	fwrite($triki, utf8_decode($parser['content']));
 	fclose($triki);
-	die('Export to '.$to_name.' done. (Dont be idiot use wordpad or better to open it, not notepad.)');
 }
 
 ?>
